f14e496930e1c11a7ebea4e2156e7c05aa6fbace
Administrator <2906741334@qq.com>
2017-01-16 16:33:53 +0800

    123

--- MainActivity.java
+++ MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String odd= name;
-        int age =20;
+        int age =2000;
     }
 }
